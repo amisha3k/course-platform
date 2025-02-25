20b454ad8c8f07f0386f1077a52c30f15923cb7d
Amisha Khichariya <amishaak38@gmail.com>
2025-02-25 19:18:57 +0530

    makemigrations

--- src/courses/models.py
+++ src/courses/models.py
@@ -19,13 +19,39 @@ updated	Stores last modification date
   -access:
        -anyone
        -email required
-       -purchase required
-       -user required
   -status:
        -published
        -coming soon
        -draft    
 
 """
+
+class AccessRequirement(models.TextChoices):
+     ANYONE="any", "Anyone"
+     EMAIL_REQUIRED="email","Email required"
+
+class PublishStatus(models.TextChoices):
+     PUBLISHED="publish", "Published" 
+     COMING_SOON="soon","coming soon"    
+     DRAFT="draft", "Draft"
+
+def handle_upload(instance,filename):
+     return f"{filename}"     
+
 class Course(models.Model):
      title=models.CharField(max_length=120)
+     description=models.TextField(blank=True, null=True)
+     image=models.ImageField()
+     access=models.CharField(
+                     max_length=5,
+                     choices=AccessRequirement.choices,
+                     default=AccessRequirement.EMAIL_REQUIRED
+                     )
+     status=models.CharField(
+                     max_length=10,
+                     choices=PublishStatus.choices,
+                     default=PublishStatus.DRAFT
+                     )
+     @property
+     def is_published(self):
+          return self.status == PublishStatus.PUBLISHED                
